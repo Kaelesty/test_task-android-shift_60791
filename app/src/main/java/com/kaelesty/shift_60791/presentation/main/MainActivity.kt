ced9ae4e8543b40ced830cab698fafa3063005b7
Ilya Bundelev <kayrismerc@yandex.ru>
2024-03-10 12:29:38 +0300

    Added notifications

--- app/src/main/java/com/kaelesty/shift_60791/presentation/main/MainActivity.kt
+++ app/src/main/java/com/kaelesty/shift_60791/presentation/main/MainActivity.kt
@@ -1,33 +1,60 @@
 package com.kaelesty.shift_60791.presentation.main
 
+import android.app.Activity
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.drawable.Icon
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.kaelesty.shift_60791.ShiftApplication
 import com.kaelesty.shift_60791.presentation.ViewModelFactory
 import com.kaelesty.shift_60791.presentation.theme.SHIFT60791Theme
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import okhttp3.internal.notify
 import javax.inject.Inject
 
 class MainActivity : ComponentActivity() {
+
+	private val NOTIFICATION_CHANNEL_ID = "42"
+	private val NOTIFICATION_CHANNEL_NAME = "Main"
 
 	private val component by lazy {
 		(application as ShiftApplication).component
 	}
 
-	@Inject lateinit var viewModelFactory: ViewModelFactory
+	@Inject
+	lateinit var viewModelFactory: ViewModelFactory
+
+	private lateinit var viewModel: MainViewModel
+
+	private val scope = CoroutineScope(Dispatchers.IO)
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 
 		component.inject(this@MainActivity)
 
+		viewModel = viewModelFactory.create(MainViewModel::class.java)
+
 		super.onCreate(savedInstanceState)
+
+		scope.launch {
+			viewModel.notificationFlow.collect {
+				sendNotification("Shift", it)
+			}
+		}
+
 		setContent {
 			SHIFT60791Theme {
 				MainScreen(
@@ -38,7 +65,7 @@ class MainActivity : ComponentActivity() {
 					onEmailClicked = {
 						runEmail(it)
 					},
-					onPhoneClicked = { runPhone(it) }
+					onPhoneClicked = { runPhone(it) },
 				)
 			}
 		}
@@ -52,9 +79,8 @@ class MainActivity : ComponentActivity() {
 					Uri.parse("tel:${phone}")
 				)
 			)
-		}
-		catch (e: Exception) {
-			Toast.makeText(this@MainActivity, "Can't open phone app", Toast.LENGTH_LONG).show()
+		} catch (e: Exception) {
+			sendNotification("Shift", "Can't open phone app")
 		}
 	}
 
@@ -66,9 +92,8 @@ class MainActivity : ComponentActivity() {
 					Uri.fromParts("mailto", email, null)
 				)
 			)
-		}
-		catch (e: Exception) {
-			Toast.makeText(this@MainActivity, "Can't open email app", Toast.LENGTH_LONG).show()
+		} catch (e: Exception) {
+			sendNotification("Shift", "Can't open email app")
 		}
 	}
 
@@ -80,9 +105,37 @@ class MainActivity : ComponentActivity() {
 					Uri.parse("geo:${latitude},${longitude}?q=${address}")
 				)
 			)
+		} catch (e: Exception) {
+			sendNotification("Shift", "Can't open navigation app")
 		}
-		catch (e: Exception) {
-			Toast.makeText(this@MainActivity, "Can't open navigation app", Toast.LENGTH_LONG).show()
+	}
+
+	private fun sendNotification(title: String, text: String) {
+		var builder = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+			val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+			val notificationChannel = NotificationChannel(
+				NOTIFICATION_CHANNEL_ID,
+				NOTIFICATION_CHANNEL_NAME,
+				NotificationManager.IMPORTANCE_DEFAULT
+			)
+			notificationManager.createNotificationChannel(notificationChannel)
+			NotificationCompat.Builder(this@MainActivity, NOTIFICATION_CHANNEL_ID)
+		} else {
+			NotificationCompat.Builder(this@MainActivity)
+		}
+
+		builder
+			.setContentTitle(title)
+			.setSmallIcon(android.R.drawable.sym_def_app_icon)
+			.setContentText(text)
+			.setPriority(NotificationCompat.PRIORITY_HIGH)
+
+		if (ActivityCompat.checkSelfPermission(
+				this,
+				android.Manifest.permission.POST_NOTIFICATIONS
+			) == PackageManager.PERMISSION_GRANTED
+		) {
+			NotificationManagerCompat.from(this@MainActivity).notify(43, builder.build())
 		}
 	}
 }
--- app/src/main/java/com/kaelesty/shift_60791/presentation/main/MainViewModel.kt
+++ app/src/main/java/com/kaelesty/shift_60791/presentation/main/MainViewModel.kt
@@ -10,7 +10,9 @@ import com.kaelesty.shift_60791.domain.ReloadUsersUseCase
 import com.kaelesty.shift_60791.domain.entities.User
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -26,14 +28,19 @@ class MainViewModel @Inject constructor(
 	private val _userFlow: MutableStateFlow<User> = MutableStateFlow(User(0))
 	val userFlow: StateFlow<User> get() = _userFlow
 
+	private val _notificationFlow: MutableSharedFlow<String> = MutableSharedFlow()
+	val notificationFlow: SharedFlow<String> get() = _notificationFlow
+
 	fun loadUsers(reloadIfNotEmpty: Boolean = false) {
 		viewModelScope.launch(Dispatchers.IO) {
 			val users = getUsersUseCase()
 
 			_usersFlow.emit (
 				if (users.isNotEmpty() && !reloadIfNotEmpty) {
+					_notificationFlow.emit("Loading data from database...")
 					users
 				} else {
+					_notificationFlow.emit("Loading data from internet...")
 					reloadUsersUseCase()
 					getUsersUseCase()
 				}
@@ -43,6 +50,7 @@ class MainViewModel @Inject constructor(
 
 	fun setUserToShowDetails(user: User) {
 		viewModelScope.launch(Dispatchers.IO) {
+			_notificationFlow.emit("Loading user ${user.username}")
 			_userFlow.emit(user)
 		}
 	}
